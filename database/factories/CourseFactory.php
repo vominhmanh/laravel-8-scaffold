521d4005404eb97d2b97454264ef6a9b1fed51cb
Minh Manh Vo <vominhmanh29@gmail.com>
2021-09-27 23:21:25 +0700

    phpcbf fixer

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -27,7 +27,7 @@ class CourseFactory extends Factory
             'price' => $this->faker->numberBetween(100, 999) * 1000,
             'start_date' => $this->faker->date('Y-m-d', 'now'),
             'finish_date' => $this->faker->date('Y-m-d', 'now'),
-            'description' => $this->faker->paragraph(3,  true),
+            'description' => $this->faker->paragraph(3, true),
             'teacher_name' => $this->faker->name(),
             'logo' => $this->faker->imageUrl(90, 90),
         ];
