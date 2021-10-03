d3810bff4251e708159d4f49a037dfb1bc5d93ef
Minh Manh Vo <vominhmanh29@gmail.com>
2021-10-03 17:47:47 +0700

    update

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -142,6 +142,7 @@ class Course extends Model
 
     public function scopeSuggestion($query, $request = null)
     {
-        return $query->ratings('desc')->limit(5);
+        //return $query->ratings('desc')->limit(5);
+        return $query->limit(5);
     }
 }
